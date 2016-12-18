a80f3c3e8486ec3d97e93c63b241756d3e00d058
Michelly de Jesus Teixeira <midejesus@poli.ufrj.br>
2016-12-18 14:51:35 -0200

    acrescentando css

--- js/trabalhando-xml.js
+++ js/trabalhando-xml.js
@@ -1,24 +1,33 @@
 /**
  * Created by midej on 18/12/2016.
  */
-function loadXMLDoc() {
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            myFunction(this);
-        }
-    };
     xmlhttp.open("GET", 'pacientes.xml', true);
     xmlhttp.send();
-}
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            listaPacientes(this);
+        }
+    };
 
-function myFunction(xml) {
-    var x, i, xmlDoc, txt;
+function listaPacientes(xml) {
+    var pacientes, i, xmlDoc, txt;
     xmlDoc = xml.responseXML;
     txt = "";
-    x = xmlDoc.getElementsByTagName("paciente");
-    for (i = 0; i< x.length; i++) {
-        txt += x[i].nodeValue + "<br>";
+    pacientes = xmlDoc.getElementsByTagName('paciente');
+
+ /*   for (i=0; i<pacientes[0].childElementCount;i++){
+        console.log(pacientes[0].children[i].tagName);
+        console.log(pacientes[0].children[i].innerHTML);*/
+   // }
+    for (i=0; i<pacientes.length;i++){
+        if (pacientes[i].children[0].innerHTML != ""){
+            txt += pacientes[i].children[0].innerHTML + "<br />";
+        }
+        else{
+            txt += "Sem número" + "<br />";
+        }
+
     }
     document.getElementById("demo").innerHTML = txt;
 }
